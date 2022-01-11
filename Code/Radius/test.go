955278710915cdcff37af379a36de25450975b3a
thteam <thteam47@gmail.com>
2022-01-11 12:02:05 +0700

    update logintc

--- Code/Radius/test.go
+++ Code/Radius/test.go
@@ -1,7 +1,16 @@
 package main
 
-func main()  {
-	fmt.Println("ok")
+import (
+	"fmt"
+	"layeh.com/radius"
+	"layeh.com/radius/rfc2865"
+)
+
+func main() {
+	packet := radius.New(radius.CodeAccessRequest, []byte(`secret`))
+	rfc2865.UserName_SetString(packet, "tim")
+	rfc2865.UserPassword_SetString(packet, "12345")
+	fmt.Println(rfc2865.NASIPAddress_Get(packet))
+	fmt.Println(rfc2865.NASPort_Strings)
+	fmt.Println(rfc2865.NASPort_Get(packet))
 }
-
-
--- Code/test/go.mod
+++ Code/test/go.mod
@@ -1,3 +1,8 @@
 module thteam
 
 go 1.12
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	layeh.com/radius v0.0.0-20210819152912-ad72663a72ab
+)
